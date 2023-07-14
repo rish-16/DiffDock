2fb3a87100ce51f28f450abee8e1b5ba8c949508
Rishabh Anand <mail.rishabh.anand@gmail.com>
2023-07-14 12:50:24 +0100

    Add download script for GPCR Typ A

---
+++ gpcr_examples/download_gpcr.sh
@@ -0,0 +1,228 @@
+# GPCRmd dataset > Class A (Rhodopsin) > Aminergic receptors > Adrenoceptors
+
+mkdir 926
+cd 926
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/18057_dyn_926.pdb
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/16630_trj_926.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/16631_trj_926.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/16632_trj_926.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/18058_oth_926.tar.gz
+tar -xvzf *.tar.gz
+rm *.tar.gz
+cd ../
+
+mkdir 100
+cd 100
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/10957_dyn_100.pdb
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/10953_trj_100.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/10954_trj_100.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/10955_trj_100.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/10956_oth_100.tar.gz
+tar -xvzf *.tar.gz
+rm *.tar.gz
+cd ../
+
+mkdir 82
+cd 82
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/10804_dyn_82.pdb
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/10799_trj_82.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/10800_trj_82.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/10801_trj_82.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/10802_oth_82.tar.gz
+tar -xvzf *.tar.gz
+rm *.tar.gz
+cd ../
+
+mkdir 88
+cd 88
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/10853_dyn_88.pdb
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/10850_trj_88.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/10851_trj_88.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/10852_trj_88.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/10856_oth_88.tar.gz
+tar -xvzf *.tar.gz
+rm *.tar.gz
+cd ../
+
+mkdir 11
+cd 11
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/10189_dyn_11.pdb
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/10185_trj_11.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/10186_trj_11.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/10187_trj_11.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/11814_oth_11.tar.gz
+tar -xvzf *.tar.gz
+rm *.tar.gz
+cd ../
+
+mkdir 116
+cd 116
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/11109_dyn_116.pdb
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/11105_trj_116.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/11106_trj_116.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/11107_trj_116.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/11103_oth_116.tar.gz
+tar -xvzf *.tar.gz
+rm *.tar.gz
+cd ../
+
+mkdir 863
+cd 863
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/17940_dyn_863.pdb
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/16210_trj_863.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/16211_trj_863.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/16212_trj_863.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/17941_oth_863.tar.gz
+tar -xvzf *.tar.gz
+rm *.tar.gz
+cd ../
+
+mkdir 763
+cd 763
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/17751_dyn_763.pdb
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/15525_trj_763.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/15526_trj_763.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/15527_trj_763.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/17752_oth_763.tar.gz
+tar -xvzf *.tar.gz
+rm *.tar.gz
+cd ../
+
+mkdir 114
+cd 114
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/11089_dyn_114.pdb
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/11085_trj_114.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/11086_trj_114.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/11087_trj_114.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/11083_oth_114.tar.gz
+tar -xvzf *.tar.gz
+rm *.tar.gz
+cd ../
+
+mkdir 1003
+cd 1003
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/18201_dyn_1003.pdb
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/17144_trj_1003.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/17145_trj_1003.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/17146_trj_1003.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/18202_oth_1003.tar.gz
+tar -xvzf *.tar.gz
+rm *.tar.gz
+cd ../
+
+mkdir 970
+cd 970
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/18139_dyn_970.pdb
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/16923_trj_970.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/16924_trj_970.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/16925_trj_970.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/18140_oth_970.tar.gz
+tar -xvzf *.tar.gz
+rm *.tar.gz
+cd ../
+
+mkdir 979
+cd 979
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/18153_dyn_979.pdb
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/16976_trj_979.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/16977_trj_979.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/16978_trj_979.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/18154_oth_979.tar.gz
+tar -xvzf *.tar.gz
+rm *.tar.gz
+cd ../
+
+mkdir 96
+cd 96
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/10919_dyn_96.pdb
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/10916_trj_96.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/10917_trj_96.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/10918_trj_96.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/10921_oth_96.tar.gz
+tar -xvzf *.tar.gz
+rm *.tar.gz
+cd ../
+
+mkdir 829
+cd 829
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/17874_dyn_829.pdb
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/15972_trj_829.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/15973_trj_829.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/15974_trj_829.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/17875_oth_829.tar.gz
+tar -xvzf *.tar.gz
+rm *.tar.gz
+cd ../
+
+mkdir 751
+cd 751
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/17727_dyn_751.pdb
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/15441_trj_751.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/15442_trj_751.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/15443_trj_751.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/17728_oth_751.tar.gz
+tar -xvzf *.tar.gz
+rm *.tar.gz
+cd ../
+
+mkdir 159
+cd 159
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/11495_dyn_159.pdb
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/11490_trj_159.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/11491_trj_159.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/11492_trj_159.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/11493_oth_159.tar.gz
+tar -xvzf *.tar.gz
+rm *.tar.gz
+cd ../
+
+mkdir 113
+cd 113
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/11077_dyn_113.pdb
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/11073_trj_113.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/11074_trj_113.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/11075_trj_113.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/11071_oth_113.tar.gz
+tar -xvzf *.tar.gz
+rm *.tar.gz
+cd ../
+
+mkdir 107
+cd 107
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/11020_dyn_107.pdb
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/11015_trj_107.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/11016_trj_107.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/11017_trj_107.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/11018_oth_107.tar.gz
+tar -xvzf *.tar.gz
+rm *.tar.gz
+cd ../
+
+mkdir 65
+cd 65
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/10651_dyn_65.pdb
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/10647_trj_65.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/10648_trj_65.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/10649_trj_65.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/10650_oth_65.tar.gz
+tar -xvzf *.tar.gz
+rm *.tar.gz
+cd ../
+
+mkdir 114
+cd 114
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/11089_dyn_114.pdb
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/11085_trj_114.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/11086_trj_114.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/11087_trj_114.xtc
+curl -O https://submission.gpcrmd.org/dynadb/files/Dynamics/11083_oth_114.tar.gz
+tar -xvzf *.tar.gz
+rm *.tar.gz
+cd ../
+
+# TODO 
+# https://submission.gpcrmd.org/dynadb/dynamics/id/684/
+# https://submission.gpcrmd.org/dynadb/dynamics/id/751/
+# https://submission.gpcrmd.org/dynadb/dynamics/id/769/
+# https://submission.gpcrmd.org/dynadb/dynamics/id/951/
+# https://submission.gpcrmd.org/dynadb/dynamics/id/962/
