6dc8b3d4e3a9400fb25ada7139f4d1173e49c156
Rishabh Anand <mail.rishabh.anand@gmail.com>
2023-07-19 22:27:00 +0100

    Updates

--- ss2.py
+++ ss2.py
@@ -29,9 +29,7 @@ pdb_ids = set(pdb_ids)
 common1 = list(pdbbind_structures.intersection(pdb_ids))
 
 test_pdb_ids = ["3nya", "3ny8", "5d6l", "4ldo"]
-# test_pdb_ids = ["5d6l"]
 test_gpcrmd_ids = [100, 88, 116, 962]
-# test_gpcrmd_ids = [116]
 
 pairs = [
     ["3NY8", "gpcr_tests/3NY8_5089/10853_dyn_88_apo.pdb", "gpcr_tests/3NY8_5089/11186_dyn_123_holo.pdb", "gpcr_tests/3NY8_5089/11178_mol_5089.sdf"],
@@ -40,16 +38,31 @@ pairs = [
     ["4LDO", "gpcr_tests/4LDO_5080/11109_dyn_116_apo.pdb", "gpcr_tests/4LDO_5080/11159_dyn_117_holo.pdb", "gpcr_tests/4LDO_5080/11122_mol_5080.sdf"]
 ]    
 
-for pair in pairs:
-    pdb_id, apo_fp, holo_fp, ligand_fp = pair
+table = []
 
-    apo_uni = mda.Universe(apo_fp, format="PDB") # apo from GPCRmd
-    # holo_uni = mda.Universe("GPCRmd/2rh1_apo.pdb", format="PDB") # apo from new beta2
-    holo_uni = mda.Universe(holo_fp, format="PDB") # apo from new beta2
+for i, pairA in enumerate(pairs):
+    temp = []
+    pdb_idA, apo_fpA, holo_fpA, ligand_fpA = pairA
+    for j, pairB in enumerate(pairs):
+        if i != j:
+            pdb_idB, apo_fpB, holo_fpB, ligand_fpB = pairB
 
-    apo_protein = apo_uni.select_atoms("protein and name CA")
-    holo_protein = holo_uni.select_atoms("protein and name CA")
-    print (apo_protein.positions.shape, holo_protein.positions.shape)
+            apo_uni = mda.Universe(apo_fpA, format="PDB") # apo from GPCRmd
+            holo_uni = mda.Universe(apo_fpB, format="PDB") # apo from new beta2
 
-    r = rms.rmsd(apo_protein.positions, holo_protein.positions, center=True, superposition=True)
-    print (f"Apo-Holo RMSD for {pdb_id}: {r}")
+            apo_protein = apo_uni.select_atoms("protein and name CA")
+            holo_protein = holo_uni.select_atoms("protein and name CA")
+            print (apo_protein.positions.shape, holo_protein.positions.shape)
+
+            if apo_protein.positions.shape[0] == holo_protein.positions.shape[0]:
+
+                r = rms.rmsd(apo_protein.positions, holo_protein.positions, center=True, superposition=True)
+                temp.append(r)
+            else:
+                temp.append(0)
+        else:
+            temp.append(0)
+
+    table.append(temp)
+
+pprint (table)    
