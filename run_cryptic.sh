31b8ff0008f6ad6253859b69e040b94f17308f5a
Rishabh Anand <mail.rishabh.anand@gmail.com>
2023-07-19 16:22:33 +0100

    Updates

--- run_cryptic.sh
+++ run_cryptic.sh
@@ -1,9 +1,10 @@
-python -m inference --protein_path cryptic_test/1w50_apo.pdb \
-                    --complex_name "1w50_isophthalamide" \
-                    --ligand cryptic_test/isophthalamide.sdf \
-                    --out_dir cryptic_test/ \
+python -m inference --protein_path cryptic_test/3cj0_POO/3cj0_apo.pdb \
+                    --complex_name "3cjo_POO" \
+                    --ligand_ "CN1CCC[CH](C1)CN(C)C(=O)Cn2c3cc(ccc3c(C4CCCCC4)c2c5ccccc5)C(O)=O" \
+                    --out_dir cryptic_test/3cjo_POO/ \
                     --inference_steps 20 \
                     --samples_per_complex 40 \
                     --batch_size 10 \
                     --actual_steps 18 \
                     --no_final_step_noise
+
