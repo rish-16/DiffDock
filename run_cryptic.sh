d881fd0655ea12965a8f9f82676b1b99b6a5dba2
Rishabh Anand <mail.rishabh.anand@gmail.com>
2023-07-19 14:56:40 +0100

    Updates

--- run_cryptic.sh
+++ run_cryptic.sh
@@ -1,8 +1,1 @@
-python -m inference --protein_path cryptic_test/5uoj.pdb \
-                    --ligand cryptic_test/octylglucoside.sdf \
-                    --out_dir cryptic_test/ \
-                    --inference_steps 20 \
-                    --samples_per_complex 40 \
-                    --batch_size 10 \
-                    --actual_steps 18 \
-                    --no_final_step_noise
+python -m inference --protein_path cryptic_test/5uoj.pdb --ligand cryptic_test/octylglucoside.sdf --out_dir cryptic_test/ --inference_steps 20 --samples_per_complex 40 --batch_size 10 --actual_steps 18 --no_final_step_noise
